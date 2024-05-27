63b5b1ab0e80ca1977aee7d7077543f2c6391448
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2024-05-27 19:16:31 +0530

    Add Inject Map Image this link
    Inject Map Image

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var links = [
     { text: "My portfolio", url: "https://nitinjangir013.github.io/myportfolio" },
     { text: "Portfolio", url: "https://nitinjangir013.github.io/portfolio" },
     { text: "Auto fill address", url: "https://nitinjangir013.github.io/auto-fill-address" },
+    { text: "Inject Map Image", url: "https://nitinjangir013.github.io/inject-map" },
     { text: "3D-Christmas-Tree", url: "https://nitinjangir013.github.io/3D-Christmas-Tree" },
     { text: "Calculator", url: "https://nitinjangir013.github.io/Calculator" }
 ];
